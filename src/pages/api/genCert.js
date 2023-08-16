eee217984e5ce3c5b1bb4443f4f7a04b07d788e7
mahmoud ashraf <mahmoudashraf11179@gmail.com>
2023-08-16 15:59:07 +0300

    first commit

---
+++ src/pages/api/genCert.js
@@ -0,0 +1,38 @@
+import { createCanvas, loadImage, registerFont } from 'canvas';
+import path from 'path';
+import getConfig from 'next/config';
+const { serverRuntimeConfig } = getConfig();
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET')
+    return res.status(404).json({ error: 'Only GET allowed' });
+
+  if (!req.query.name || req.query.name.length < 3)
+    return res
+      .status(400)
+      .json({ error: 'No name or name less than 3 letters' });
+
+  let fileName = path.join(process.cwd(), 'src', 'cert', 'image.png');
+  let fontName = path.join(process.cwd(), 'src', 'cert', 'font.ttf');
+  let nameText = req.query.name.toUpperCase().split(' ').slice(0, 3).join(' ');
+
+  const image = await loadImage(fileName);
+
+  registerFont(fontName, { family: '29LT Kaff' });
+
+  const canvas = createCanvas(image.width, image.height, 'pdf');
+  const ctx = canvas.getContext('2d');
+
+  ctx.drawImage(image, 0, 0);
+
+  ctx.font = '32px "29LT Kaff"';
+  ctx.fillStyle = '#11BAB1';
+  ctx.fillText(nameText, 120, 455);
+
+  await res.writeHead(200, {
+    'Content-Type': 'application/pdf',
+    'Content-Disposition': `attachment; filename="${nameText}.pdf"`,
+  });
+  const stream = canvas.createPDFStream();
+  stream.pipe(res);
+}
---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+import react, { useState, useEffect } from 'react';
+
+/* eslint-disable @next/next/no-img-element */
+export default function Home() {
+  const [name, setName] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setError('');
+  }, [name]);
+
+  const submitName = (e) => {
+    e.preventDefault();
+    if (name.split(' ').length < 3) {
+      setError('Enter 3 names');
+      return;
+    }
+    window.open(`/api/genCert?name=${name}`, '_blank');
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="container containerHome">
+        <img id="certificate-image" src="/1.png" alt="Certificate Image" />
+        <h1>
+          <strong>Luxor Conference Certificate</strong>
+        </h1>
+        <form onSubmit={submitName}>
+          <div className="form-group">
+            <label htmlFor="name">Recipient&apos;s Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <button type="submit">Generate Certificate</button>
+          {error && <p className="mt-4 text-red-500">{error}</p>}
+        </form>
+        <img id="logo" src="/2.png" alt="Conference Logo" />
+      </div>
+    </div>
+  );
+}
